f7ae4ace491cb7c725fdf26883416bb3391d5c16
gyg <ganyaguang@dakele.com>
2013-11-30 13:20:00 +0800

    modify android ctags script

--- scripts/mkvimforandr.sh
+++ scripts/mkvimforandr.sh
@@ -3,22 +3,16 @@
 #function: make cscope database and ctags database for android source
 
 ignore="( -name .git -o -name test -o -name tests ) -prune -o"
-type="( -name *.java -o -name *.cpp -o -name *.[ch] -o -name *.xml -o -name
+type="( -name *.java -o -name *.cpp -o -name *.[ch] -o -name
 *.aidl )"
 
 android=$ANDROID_SRC
 
 path="$android/frameworks \
-    $android/bionic \
-    $android/bootable \
-    $android/dalvik \
-    $android/external \
     $android/hardware \
     $android/kernel \
     $android/mediatek \
     $android/packages \
-    $android/prebuilt/linux-x86 \
-    $android/system \
     $android/vendor"
 
 cscopedb=$ANDROID_SRC/cscope
@@ -83,12 +77,25 @@ dotags()
     fi  
 }
 
+dolookups()
+{
+    # generate tag file for lookupfile plugin
+    echo "!_TAG_FILE_SORTED	2	/2=foldcase/" > $cscopedb/filenametags
+    for src in $path
+    do
+        find -L $src $ignore $type      \
+            -printf "%f\t%p\t1\n"       \
+            | sort -f >> $cscopedb/filenametags
+    done
+}
+
 usage() 
 {
     echo "Usages:"
     echo "cscope  : make cscope database only"
     echo "tags    : make ctags database only"
-    echo "all     : make two databases"
+    echo "lookup  : make lookup list only"
+    echo "all     : make two databases and lookup list"
 }
 
 case $1 in
@@ -102,10 +109,16 @@ case $1 in
         dotags
         ;;  
 
+    "lookup")
+        prepare
+        dolookups
+        ;;  
+
     "all")
         prepare
         docscope
         dotags
+        dolookups
         ;;
     "")
         usage
